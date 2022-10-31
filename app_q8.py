548051b49e5f4de2228331a7ae6d7afb6bce4a85
deepika0196 <deepika15997@gmail.com>
2022-10-31 12:53:46 +0530

    modified

---
+++ app_q8.py
@@ -0,0 +1,26 @@
+""" #a
+s= "PURA VIDA"
+print(s[9]+ s[9:15])
+
+#b
+s= "PURA VIDA"
+s1=s[:10]+ s[10:]
+print(s1)
+s2=s[10]+s[-10]
+print(s2)
+"""
+#c
+s= "PURA VIDA"
+s1=s*2
+s2=s1[-19]+s1[-20]
+s3=s1[-19:]
+print(s1)
+
+#d
+s= "PURA VIDA"
+s1=s[:5]
+s2=s[5:]
+s3=s1*s2
+s4=s2+'3'
+s5=s1+3
+
